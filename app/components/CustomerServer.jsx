3dcc8fe88200a8e24058888aea446adb0a0ac143
Adil Yerlan <adilyerlan71@gmail.com>
2024-01-30 21:28:01 +0600

    some code correction

--- app/components/CustomerServer.jsx
+++ app/components/CustomerServer.jsx
@@ -1,20 +1,4 @@
 import React from 'react'
-import { datas } from './data'
 
 export const CustomerServer = () => {
-    const people = datas;
-    return (
-        <div>
-            {people.map(person => {
-                const {id, name, age} = person;
-                return(
-                    <div key={id}>
-                        {id} <br /> {name}, {age}
-                    </div>
-
-                )
-            })
-            }
-        </div>
-    )
 }
